4de65c93dd5aafce27b6e0dfc15e8a5dc1a74803
Karam <karamhegazy2008@gmail.com>
2024-10-07 00:14:27 +0300

    some changes

---
+++ app/src/main/java/com/example/myapplicationniggers/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplicationniggers;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+private EditText X ;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        X = findViewById(R.id.Text) ;
+    }
+    public  void changetext (View view) {
+        X.setText(" my first class ");
+    }public void finish (View view) {
+        finish();
+        System.exit(0);
+    }
+}
